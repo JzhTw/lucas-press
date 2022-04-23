2d351d26689f1709f56042e88e8140409c97d84d
JEFF <qaawssedd456@gmail.com>
2022-04-23 15:06:21 +0800

    製作手機板

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,65 +1,87 @@
 <template>
-    <div class="flex min-h-screen">
+    <div class="flex flex-col sm:flex-row min-h-screen">
         <!-- 左側選單 -->
-        <div class="w-64 flex flex-col justify-between bg-slate-600 text-white">
-            <div>
-                <!-- Logo -->
-                <div class="flex justify-center items-center h-16">
+        <div class="flex flex-col justify-between flex-shrink-0 bg-slate-600 text-white w-full sm:w-64">
+            <!-- logo -->
+            <div class="flex justify-between items-center px-4 h-16 sm:justify-center">
+                <div class="flex justify-cneter items-center">
                     <heroicons-outline-book-open class="w-10 h-10 mr-2" />
                     <span class="text-xl font-medium">LucasPress</span>
                 </div>
 
+                <button class="justify-center items-center w-8 h-8 sm:hidden" @click="toggleMenu">
+                    <heroicons-outline-menu class="w-5 h-5"/>
+                </button>
+            </div>
+            <div class="flex-grow sm:flex sm:flex-col justify-between" :class="showMenu ? '' : 'hidden'">
                 <!-- 主要選單 -->
                 <ul>
                     <li>
                         <RouterLink to="/" class="flex items-center px-5 py-3 text-white">
-                            <heroicons-outline-home class="w-4 h-4" />
+                            <heroicons-outline-home class="w-5 h-5 mr-2" />
                             首頁
                         </RouterLink>
                     </li>
                     <li>
                         <RouterLink to="/" class="flex items-center px-5 py-3 text-slate-400 hover:text-white">
-                            <heroicons-outline-document-text class="w-4 h-4" />
+                            <heroicons-outline-document-text class="w-5 h-5 mr-2" />
                             文章
                         </RouterLink>
                     </li>
                     <li>
                         <RouterLink to="/" class="flex items-center px-5 py-3 text-slate-400 hover:text-white">
-                            <heroicons-outline-user class="w-4 h-4" />
+                            <heroicons-outline-user class="w-5 h-5 mr-2" />
                             個人資料
                         </RouterLink>
                     </li>
                 </ul>
-            </div>
-            <!-- 用戶名稱 -->
-            <div class="flex justify-between items-center border-b px-5 py-4 border-t border-slate-400">
-                <div class="flex items-center">
-                    <img class="w-8 h-8 rouded-full mr-2"
-                        src="https://cdn.jsdelivr.net/npm/slidev-theme-ycs77/public/images/lucas_v_avatar.jpg" alt="">
-                    <div class="font-medium tracking-wide">Lucas 洛可</div>
+                <!-- 用戶名稱(手機板) -->
+                <div class="mt-2 py-1 border-t border-slate-400 sm:hidden" >
+                    <div class="flex items-center px-4 py-3">
+                        <img class="w-8 h-8 rouded-full mr-2"
+                            src="https://cdn.jsdelivr.net/npm/slidev-theme-ycs77/public/images/lucas_v_avatar.jpg"
+                            alt="">
+                        <div class="font-medium tracking-wide">Lucas 洛可</div>
+                    </div>
                 </div>
+                <!-- 用戶名稱(電腦版)-->
+                <div class="hidden sm:flex justify-between sm:items-center border-b px-5 py-4 border-t border-slate-400">
+                    <div class="flex items-center">
+                        <img class="w-8 h-8 rouded-full mr-2"
+                            src="https://cdn.jsdelivr.net/npm/slidev-theme-ycs77/public/images/lucas_v_avatar.jpg"
+                            alt="">
+                        <div class="font-medium tracking-wide">Lucas 洛可</div>
+                    </div>
 
-                <button type="button"
-                    class="flex justify-center item-center w-7 h-7 hover:bg-slate-400 rounded transition-colors duration-100">
-                    <heroicons-outline-dots-vertical class="w-4 h-4" />
-                </button>
+                    <button type="button"
+                        class="flex justify-center items-center w-7 h-7 hover:bg-slate-400 rounded transition-colors duration-100">
+                        <heroicons-outline-dots-vertical class="w-4 h-4" />
+                    </button>
+                </div>
             </div>
-
         </div>
 
 
         <!-- 右側內容區 -->
         <div class="flex-grow min-w-0">
-            <slot>
-
-            </slot>
+            <slot></slot>
+            <div class="overflow-x-auto">
+                <pre>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis aliquid, optio fugiat magni soluta reprehenderit obcaecati eius facilis aperiam velit commodi. Deleniti voluptate aperiam quos magni at molestias aliquid earum?
+                </pre>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
-
+    setup() {
+        const showMenu = ref(false)
+        const toggleMenu = () => showMenu.value = !showMenu.value
+        return { showMenu, toggleMenu }
+    }
 }
 </script>
 
